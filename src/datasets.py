d63bdfcbb41ef8ee08400c62f25b5e4eea0ed0cb
Ben <b29308188@gmail.com>
2015-08-18 18:26:54 +0800

    add basic datasets.py

---
+++ src/datasets.py
@@ -0,0 +1,28 @@
+import sys
+import numpy as np
+
+def train_test_split_order(input_file, output_train, output_test):
+    A = []
+    for line in open(input_file, "r"):
+        tokens = line.strip().split("::")
+        A.append((tokens[0], tokens[1], tokens[2], int(tokens[3])))
+    A.sort(key = lambda x: x[3])
+    train_A = A[:int(len(A)*0.8)]
+    test_A = A[int(len(A)*0.8):]
+    with open(output_train, "w") as f:
+        for a in train_A:
+            f.write("%s %s %s %d\n"%(a[0], a[1], a[2], a[3]))
+    with open(output_test, "w") as f:
+        for a in test_A:
+            f.write("%s %s %s %d\n"%(a[0], a[1], a[2], a[3]))
+
+def read_instances(train_file, test_file):
+    trainI = []
+    testI = []
+    for line in open(train_file):
+        tokens = line.strip().split()
+        trainI.append((tokens[0], tokens[1], tokens[2]))
+    for line in open(test_file):
+        tokens = line.strip().split()
+        testI.append((tokens[0], tokens[1], tokens[2]))
+    return trainI, testI
